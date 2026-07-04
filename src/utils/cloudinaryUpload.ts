31480930d7600c597c02c7512cff72c885dbc4bd
Radian-syahputra <driftwear26@gmail.com>
2026-07-04 15:45:30 +0700

    set and create utils cloudinaryUpload

---
+++ src/utils/cloudinaryUpload.ts
@@ -0,0 +1,13 @@
+import cloudinary from "../config/cloudinary";
+
+
+const cloudinaryUpload  = (buffer : Buffer) : Promise<{imageUrl : string ; imagePublicId : string}> => {
+    return new Promise((resolve, reject) => {
+        cloudinary.uploader.upload_stream({folder : 'menu'}, (error, result) => {
+            if(error) return reject(error);
+            else resolve({imageUrl : result!.secure_url!, imagePublicId : result!.public_id!})
+        }).end(buffer)
+    })
+}
+
+export default cloudinaryUpload
