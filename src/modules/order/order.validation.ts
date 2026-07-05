41df4a641d200081e7a37339d415992dc11fff77
Radian-syahputra <driftwear26@gmail.com>
2026-07-05 19:31:13 +0700

    create order validation

---
+++ src/modules/order/order.validation.ts
@@ -0,0 +1,12 @@
+import z from "zod";
+
+export const orderItemSchema = z.object({
+    menuId : z.string().min(1, 'Menu Wajib Di isi'),
+    quantity : z.coerce.number().int('Qantity Harus Bertipe Number').min(1, "Minimal Pesan 1"),
+    note : z.string().optional()
+})
+
+export const createOrderSchema = z.object({
+  tableId: z.string().min(1, "Pilih Meja"),
+  items: z.array(orderItemSchema).min(1, "Order Item Wajib Di isi")
+})
