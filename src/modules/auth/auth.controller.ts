62a2dfb74ac1b021b44ddc64e52bb44a72184c44
Radian-syahputra <driftwear26@gmail.com>
2026-07-10 16:18:29 +0700

    prepare for deployment

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -15,14 +15,14 @@ export const registerController = async (req: Request, res: Response) => {
 
 export const loginController = async (req: Request, res: Response) => {
   try {
-    const { email, password } = req.body
+    const { email, password } = req.body;
 
     const { token, user } = await loginService(email, password);
 
     res.cookie("token", token, {
       httpOnly: true,
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 hari
     });
 
@@ -36,8 +36,8 @@ export const logoutController = (req: Request, res: Response) => {
   try {
     res.clearCookie("token", {
       httpOnly: true,
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
     });
     return successResponse(res, "Logout Berhasil");
   } catch (error: any) {
@@ -45,17 +45,16 @@ export const logoutController = (req: Request, res: Response) => {
   }
 };
 
-
-export const getMeController = async (req : AuthRequest, res : Response) => {
-    try {
-        const userId = req.user?.id
-        if(!userId) {
-            return errorResponse(res, "Unauthorized", 401)
-        }
-
-        const user = await getMeService(userId)
-        return successResponse(res, "User ditemukan", user)
-    } catch (error : any) {
-        return errorResponse(res, error.message);
+export const getMeController = async (req: AuthRequest, res: Response) => {
+  try {
+    const userId = req.user?.id;
+    if (!userId) {
+      return errorResponse(res, "Unauthorized", 401);
     }
-}
+
+    const user = await getMeService(userId);
+    return successResponse(res, "User ditemukan", user);
+  } catch (error: any) {
+    return errorResponse(res, error.message);
+  }
+};
